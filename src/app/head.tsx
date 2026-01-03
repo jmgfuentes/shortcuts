113fb3eb2aadbd3acb14c4ddff3fc40bb37d55a3
Jesús Manuel González Fuentes <jmgfuentes@iniciablog.com>
2026-01-03 18:58:55 +0000

    Forzar tema oscuro: fondo negro global, meta tags y correcciones de estilos

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="theme-color" content="#000000" />
+      <meta name="color-scheme" content="dark" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="mobile-web-app-capable" content="yes" />
+    </>
+  );
+}
